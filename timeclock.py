6b9b0c11f5f88b98a9c57af20f81bd39e22a9994
Pratik <upballoon@ThinkpadX1C.localdomain>
2022-04-21 01:02:39 -0600

    fixed formatting errors

--- timeclock.py
+++ timeclock.py
@@ -84,4 +84,4 @@ def update_output(clkIn_clicks,clkOut_clicks):
 
 
 if __name__ == '__main__':
-    app.run_server(host=‘0.0.0.0’,debug=True, port=8050)
+    app.run_server(host='0.0.0.0',debug=True, port=8050)
